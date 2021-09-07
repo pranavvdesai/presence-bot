ae2fa6d6625ebd732773ba1508a74d20d4a8eace
pranavvdesai <pranavdesai.psd@gmail.com>
2021-09-07 21:48:50 +0530

    db connected

--- index.js
+++ index.js
@@ -35,4 +35,35 @@ client.on('ready', () =>
 	+ `> Servers: ${client.guilds.cache.size}`));
 	console.log('─────────────────────────────────────────────');
 	client.user.setActivity(`${PREFIX}shop`, {type: 'WATCHING'});
+
+		// Check Databases
+		conn.connect(err => {
+			if(err) throw err;
+			console.log("- Database Connected.");
+			console.log("- Checking if Msgs Table exist...");
+			conn.query(`SHOW TABLES LIKE 'Msgs'`, (err, rows) => {
+				if(err) throw err;
+				if(rows.length < 1) {
+					conn.query(`CREATE TABLE Msgs (UserID varchar(30) NOT NULL,Msgs int(11) DEFAULT 1) ENGINE=InnoDB DEFAULT CHARSET=latin1;`);
+					console.log("- Msgs Database Built.");
+					console.log("- Msgs database will be where the user messages count will be stored.");
+				} else {
+					console.log("- Msgs Database Exists.");
+				}
+			});
+			console.log('─────────────────────────────────────────────');
+			conn.query(`SHOW TABLES LIKE 'Activity'`, (err, rows) => {
+				if(err) throw err;
+				if(rows.length < 1) {
+					conn.query(`CREATE TABLE Activity (ID int(11) NOT NULL,UserID varchar(30) NOT NULL,ChannelID varchar(25) NOT NULL,JoinTime int(11) NOT NULL,LeftTime int(11) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;`);
+					conn.query(`ALTER TABLE Activity ADD PRIMARY KEY (ID);`);
+					conn.query(`ALTER TABLE Activity MODIFY ID int(11) NOT NULL AUTO_INCREMENT;`);
+					console.log("- Activity Database Built.");
+					console.log("- Activity database will be where the voice chat time for each user will be stored.");
+				} else {
+					console.log("- Activity Database Exists.");
+				}
+			});
+			console.log('─────────────────────────────────────────────');
+		});
 });
